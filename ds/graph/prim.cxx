fdd03457402636edb6ab21ed174e3e4c02aa18ea
rajul <rajul885@yahoo.com>
2016-12-22 14:45:47 +0530

    added graph algos

---
+++ ds/graph/prim.cxx
@@ -0,0 +1,105 @@
+// STL implementation of Prim's algorithm for MST
+#include<bits/stdc++.h>
+using namespace std;
+# define INF 0x3f3f3f3f
+ 
+// iPair ==>  Integer Pair
+typedef pair<int, int> iPair;
+ 
+// This class represents a directed graph using
+// adjacency list representation
+class Graph
+{
+    int V;    // No. of vertices
+ 
+    // In a weighted graph, we need to store vertex
+    // and weight pair for every edge
+    vector< pair<int, int> > *adj;
+ 
+public:
+    Graph(int V);  // Constructor
+ 
+    // function to add an edge to graph
+    void addEdge(int u, int v, int w);
+ 
+    // Print MST using Prim's algorithm
+    void primMST();
+};
+ 
+// Allocates memory for adjacency list
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new vector<iPair> [V];
+}
+ 
+void Graph::addEdge(int u, int v, int w)
+{
+    adj[u].push_back(make_pair(v, w));
+    adj[v].push_back(make_pair(u, w));
+}
+
+typedef pair<int , int> ii;
+// Prints shortest paths from src to all other vertices
+void Graph::primMST()
+{
+    std::priority_queue<ii, vector<ii> ,greater<ii> > pq;
+    pq.push(make_pair(0, 0));
+    vector<int> parent(V,-1);
+    vector<int> dist(V, 999);
+    vector<int> taken(V, -1);
+    while(!pq.empty())
+    {
+        int u = pq.top().second;
+        taken[u] = 1;
+        //int w = pq.top().first;
+        pq.pop();
+        for(int i =0; i < adj[u].size(); i++)
+        {
+            int v = adj[u][i].first;
+            int w = adj[u][i].second;
+            if(taken[v] == -1)
+            {
+                if(dist[v] > w)
+                {
+                    dist[v] = min(dist[v], w);
+                    parent[v] = u;
+                }
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+    for(int i= 1; i < V; i ++)
+    {
+        cout<<parent[i]<<"---"<<i<<endl;
+    }
+    cout<<endl;
+}
+ 
+// Driver program to test methods of graph class
+int main()
+{
+    // create the graph given in above fugure
+    int V = 9;
+    Graph g(V);
+ 
+    //  making above shown graph
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 7, 8);
+    g.addEdge(1, 2, 8);
+    g.addEdge(1, 7, 11);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 8, 2);
+    g.addEdge(2, 5, 4);
+    g.addEdge(3, 4, 9);
+    g.addEdge(3, 5, 14);
+    g.addEdge(4, 5, 10);
+    g.addEdge(5, 6, 2);
+    g.addEdge(6, 7, 1);
+    g.addEdge(6, 8, 6);
+    g.addEdge(7, 8, 7);
+ 
+    g.primMST();
+ 
+    return 0;
+}
